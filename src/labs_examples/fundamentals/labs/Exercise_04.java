8ca065d6a319175d73b0e6ff092a4489c701f567
Jesus Martinez <jmart649@stu.southtexascollege.edu>
2021-09-14 18:32:48 -0500

    "changes made"

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -17,6 +17,27 @@ public class Exercise_04 {
 
         // write your code below
 
+        byte var = 1;
+        System.out.println("byte var is: " + var);
+
+        short var1 = 1;
+        System.out.println("short var1 is: " + var1);
+
+        long var2 = 1;
+        System.out.println("long var2 is: " + var2);
+
+        float n = 1;
+        System.out.println("float n is: " + n);
+
+        double x = 1.5;
+        System.out.println("double x is: " + x);
+
+        boolean var3 = true;
+        System.out.println("boolean var3 is: " + var3);
+
+        char alpha = 'a';
+        System.out.println("char alpha is: " + alpha);
+
     }
 
 }
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,14 +14,22 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int n = str.length() + 1;
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
+        boolean var2 = str == str2;
+
 
         // please concatenate str & str2 and set the result to a new String variable below
+        String var = str + " " + str2;
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+
+        boolean var3 = str.contains("h");
+
+        System.out.println(var3);
 
     }
 
