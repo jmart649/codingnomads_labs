d6dde022511ea2c32321bb56f8ccb349f4a7f41d
Jesus Martinez <jmart649@stu.southtexascollege.edu>
2021-10-12 14:42:05 -0500

    enums labs

--- src/labs_examples/enumerations/labs/Exercise_01.java
+++ src/labs_examples/enumerations/labs/Exercise_01.java
@@ -8,3 +8,31 @@ package labs_examples.enumerations.labs;
  *      of this enum from a seperate class.
  */
 
+public class Exercise_01 {
+    public static void main(String[] args) {
+        System.out.println("    MY SCHEDULE");
+        for(Schedule s : Schedule.values()){
+            System.out.println(s + ": " + s.getSchedule());
+        }
+    }
+}
+
+enum Schedule{
+    MONDAY("7AM - 3PM"),
+    TUESDAY("8AM - 4PM"),
+    WEDNESDAY("8AM - 4PM"),
+    THURSDAY("8AM - 4PM"),
+    FRIDAY("7AM - 3PM"),
+    SATURDAY("OFF"),
+    SUNDAY("OFF");
+
+    private final String schedule;
+
+    Schedule(String schedule){
+        this.schedule = schedule;
+    }
+
+    public String getSchedule() {
+        return schedule;
+    }
+}
